c011263d67f69050c871328402d8eb874bd0c2ed
Nick Jeffrey <nickljeffrey@gmail.com>
2012-12-12 02:44:15 -0600

    transitioning to Bluetooth

--- pong/src/com/nick/aponggame/StateHandler2P.java
+++ pong/src/com/nick/aponggame/StateHandler2P.java
@@ -3,21 +3,15 @@ package com.nick.aponggame;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.IntentFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class StateHandler2P extends SurfaceView implements 	SurfaceHolder.Callback, 
-															Runnable, 
-															NetworkStateHandler
+															Runnable
 {
 	private SurfaceHolder holder;
 	private int scoreP1;
@@ -29,10 +23,6 @@ public class StateHandler2P extends SurfaceView implements 	SurfaceHolder.Callba
 	private Thread gameThread;
 	private Activity owner;
 	
-	WifiP2pManager manager;
-	Channel channel;
-	BroadcastReceiver receiver;
-	IntentFilter intent;
 	
 //NOTE:THIS HORIZONTAL ORIENTATION RESULTS IN UNTESTABLE BEHAVIOR ATM
 	public StateHandler2P(Activity context)
@@ -57,27 +47,9 @@ public class StateHandler2P extends SurfaceView implements 	SurfaceHolder.Callba
 	   		balls.add(new Ball(155, 10, 3, 3, 10));//always starts on p1 screen [for now?]
 	   		//professional version needs a way to know height and width to set it at proper starting location, not a big deal
 	   	
-	   	//HANDLE SETTING UP NETWORK HERE
-	   	manager=(WifiP2pManager) owner.getSystemService(Context.WIFI_P2P_SERVICE);
-	   	channel=manager.initialize(owner,  owner.getMainLooper(),  null);
-	   	receiver=new WifiReceiver(manager, channel, owner);
+	   	//HANDLE SETTING UP NETWORK HERE ?
 	   	
-	   	intent=new IntentFilter();
-	   	intent.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-	   	intent.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-	   	intent.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-	   	intent.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 	   	
-	   	manager.discoverPeers(channel, new WifiP2pManager.ActionListener(){
-	   		//@Override
-	   		public void onSuccess(){
-	   			System.out.println("SUCCESSful discovery of peers");
-	   		}
-	   		//@Override
-	   		public void onFailure(int reason){
-	   			System.out.println("Discovery failed. ); reason="+reason);
-	   		}
-	   	});
 	}
 	
 	//used by run
@@ -192,19 +164,6 @@ public class StateHandler2P extends SurfaceView implements 	SurfaceHolder.Callba
 		return true;
 	}
     
-    /* getter/setters
-     * 
-     * @param - if there is a parameter, set it to the corresponding variable
-     * @return - corresponding variable
-     */
-    public BroadcastReceiver getReceiver()
-    {
-    	return receiver;
-    }
-    public IntentFilter getIntent()
-    {
-    	return intent;
-    }
     
 	
 	
